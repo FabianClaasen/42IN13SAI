92c0f0339f857ea30d5c5fb10e489a2c0f8f9922
Jim Franke <info@jimfranke.com>
2014-11-12 20:37:13 +0100

    MVC Code structure added to views. Exceptions while calling execute.

--- Development/42IN13SAi/MainWindow.cpp
+++ Development/42IN13SAi/MainWindow.cpp
@@ -19,37 +19,28 @@ MainWindow::MainWindow(QWidget *parent)
 void MainWindow::showMenuBar()
 {
 	// Create menu bar
-	QMenuBar* menu = new QMenuBar();
+	QMenuBar* menu = nullptr;
 
-	// Get the file from the stream and convert to std::string
-	std::string input(getFileFromStream().toStdString());
+	#ifdef _WIN32
+		menu = new QMenuBar();
+	#else
+		menu = new QMenuBar(0);
 
-	TokenizerController *tokenizer_controller = new TokenizerController(input);
-	try {
-		// Tokenize the code		
-		tokenizer_controller->Tokenize();
-	}
-	catch (const std::exception& e)
-	{
-		puts(e.what());
-		delete(tokenizer_controller);	
-		return;
-	}
-
-	// Run the compiler
-	Compiler compiler = Compiler(tokenizer_controller->GetCompilerTokens());
-	compiler.Compile();
+		QMenu* mainMenu = menu->addMenu("File");
+	#endif
 
 	// Create action and connect
 	runAction = menu->addAction("Run");
 	clearAction = menu->addAction("Clear console");
 
+	#ifndef _WIN32
+		mainMenu->addAction(runAction);
+		mainMenu->addAction(clearAction);
+	#endif
+
 	// Set the menu bar on ui from left to right
 	this->setMenuBar(menu);
 	menu->setLayoutDirection(Qt::LeftToRight);
-	// Run the virtual machine with the compilernodes
-	VirtualMachine virtual_machine = VirtualMachine(compiler.GetSymbolTable(), compiler.GetSubroutineTable(), compiler.GetCompilerNodes());
-	virtual_machine.ExecuteCode();
 }
 
 QAction *MainWindow::getRunAction()
@@ -65,31 +56,6 @@ QAction *MainWindow::getClearAction()
 QString MainWindow::getText()
 {
 	return codeEditor->toPlainText();
-	// Create menu bar
-    QMenuBar* menu = nullptr;
-#ifdef _WIN32
-	menu = new QMenuBar();
-#else
-    menu = new QMenuBar(0);
-    
-    QMenu* mainMenu = menu->addMenu("File");
-#endif
-
-	// Create action and connect
-	QAction *runAction = menu->addAction("Run");
-	QAction *clearAction = menu->addAction("Clear console");
-
-	connect(runAction, SIGNAL(triggered()), this, SLOT(execute()));
-	connect(clearAction, SIGNAL(triggered()), this, SLOT(clearConsole()));
-    
-#ifndef _WIN32
-    mainMenu->addAction(runAction);
-    mainMenu->addAction(clearAction);
-#endif
-
-	// Set the menu bar on ui from left to right
-	this->setMenuBar(menu);
-	menu->setLayoutDirection(Qt::LeftToRight);
 }
 
 MainWindow::~MainWindow()
