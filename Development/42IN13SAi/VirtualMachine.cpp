2c11057e4bf9ccd57d01439b02d1e037ee3cde38
Devin van Nieuwenhoven <turbodevin@hotmail.com>
2015-01-16 15:09:05 +0100

    Fixed precision limit VM toString

--- Development/42IN13SAi/VirtualMachine.cpp
+++ Development/42IN13SAi/VirtualMachine.cpp
@@ -35,7 +35,7 @@ void VirtualMachine::quit()
 std::string VirtualMachine::toString(long double number)
 {
 	std::ostringstream strs;
-	strs << std::fixed << std::setprecision(16) << number;
+	strs << std::fixed << std::setprecision(14) << number;
 	std::string str = strs.str();
 	return str;
 }
